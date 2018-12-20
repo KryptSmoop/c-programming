b2e2d230a3289ef2a1b8495b4fe9e4e87858d3af
KryptSmoop <icallhacks19@gmail.com>
2018-12-20 12:59:08 -0800

    Add files via upload

--- kernel.c
+++ kernel.c
@@ -1,8 +1,31 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main(void){
-  
-    
-  
+char * command_str;
+
+int kernel_enabled = 1;
+
+void command_parser(char * x){
+	
+	if(x == "help"){
+		printf("%s", "-help: previews this screen...");
+	}
+}
+
+void commands(){
+	printf("%s", "<C:/>");
+	scanf("%s", command_str);
+	command_parser(command_str);
+	
+}
+
+int main(){
+	while(kernel_enabled == 1)
+	{
+		
+		commands();
+				
+	}
+	
+	return 0;
 }
